c2d895352ea9b91d7b87d15d30aebf601c225ef5
Frank Gonzalez <frank.gonzalez@hardtech.co>
2021-07-29 11:02:07 -0400

    fix when retrieve or find some resources from 201 status code to 200

--- src/main/java/com/leantech/test/api/employee/create/CreateEmployeeController.java
+++ src/main/java/com/leantech/test/api/employee/create/CreateEmployeeController.java
@@ -12,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -55,7 +56,7 @@ public class CreateEmployeeController {
             @ApiResponse(code = 409, message = "Employee creation error", response = CreateEmployeeResponse.class)
     })
     @PostMapping(value = "positions/{positionId}/employees", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<CreateEmployeeResponse> createEmployee(@PathVariable("positionId") Integer positionId,
+    public ResponseEntity<CreateEmployeeResponse> createEmployee(@PathVariable("positionId") @Validated Integer positionId,
                                                                  @RequestBody CreateEmployeeRequest createEmployeeRequest) {
         log.info("CreateEmployeeController - POST - createEmployee");
 
